409a632fc68eff70279c96ed0ba818b20824609f
czfandyslash <czfandyslash@gmail.com>
2019-10-09 22:41:58 +0800

    add a js script

---
+++ test.user.js
@@ -0,0 +1,10 @@
+// ==UserScript==
+// @name         Damai-test
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  大麦抢票测试
+// @author       CZF
+// @match        https://buy.damai.cn/orderConfirm*
+// @grant        none
+// @require      https://cdn.bootcss.com/jquery/3.4.1/jquery.min.js
+// ==/UserScript==
